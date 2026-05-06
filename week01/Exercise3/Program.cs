fda6f17c324f3799901d94ab86a43b0e3286cfdb
andreas-gatio <andreas@gatioagency.com>
2026-05-06 15:36:26 +0200

    Completed Exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        Console.WriteLine("What is the magic number? (1-100) ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+
+        while (favoriteNumber != number)
+        {
+            if (favoriteNumber > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+            }
+
+        Console.Write("What is the magic number? ");
+        favoriteNumber = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("You guessed it!");
     }
 }
